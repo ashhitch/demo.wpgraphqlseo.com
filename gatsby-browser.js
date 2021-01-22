19ddac6a8bd7169ee7a137570026791694878cbd
Ash Hitchcock <hello@ashleyhitchcock.co.uk>
2021-01-22 12:52:48 +0000

    fix filename casing

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,7 +1,7 @@
 // custom typefaces
-import "typeface-montserrat"
-import "typeface-merriweather"
+import 'typeface-montserrat';
+import 'typeface-merriweather';
 // normalize CSS across browsers
-import "./src/normalize.css"
+import './src/normalize.css';
 // custom CSS styles
-import "./src/style.css"
+import './src/style.css';
